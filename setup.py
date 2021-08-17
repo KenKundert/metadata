0e8160e90c2139f546bf1a1e019c3a61622dc3c4
Ken Kundert <ken@theKunderts.net>
2021-08-17 13:58:40 -0700

    enable zip_safe in installer

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='metadata',
-    description="Edit audio file metadata.",
-    author="Ken Kundert",
-    author_email='theNurd@nurdletech.com',
+    name = 'metadata',
+    description = "Edit audio file metadata.",
+    author = "Ken Kundert",
+    author_email = 'theNurd@nurdletech.com',
     py_modules = 'metadata'.split(),
     python_requires = '>=3.6',
     install_requires = """
@@ -15,6 +15,7 @@ setup(
         shlib
         voluptuous
     """.split(),
-    scripts='mdexport mdimport'.split(),
-    license='GPLv3',
+    scripts = 'mdexport mdimport fromflac'.split(),
+    license = 'GPLv3',
+    zip_safe = True,
 )
